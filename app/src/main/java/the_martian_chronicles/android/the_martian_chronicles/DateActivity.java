d1fef60c3ff7ac9f3ba72099159e9798416d2e88
nurzhigit.shalkarbai@ce.sdu.edu.kz <asd>
2018-04-06 01:49:46 +0600

    All but row

--- app/src/main/java/the_martian_chronicles/android/the_martian_chronicles/DateActivity.java
+++ app/src/main/java/the_martian_chronicles/android/the_martian_chronicles/DateActivity.java
@@ -1,13 +1,69 @@
 package the_martian_chronicles.android.the_martian_chronicles;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DateActivity extends AppCompatActivity {
+
+    final Calendar calendar = Calendar.getInstance();
+    long from;
+    long to;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date);
+        ButterKnife.bind(this);
+        SharedPreferences preferences = getSharedPreferences("common", MODE_PRIVATE);
+        from=preferences.getLong("from",System.currentTimeMillis());
+        to=preferences.getLong("to",System.currentTimeMillis()*2);
+
     }
+
+    @OnClick(R.id.from)
+    void onFromClick(View view){
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year,month,day);
+                from=calendar.getTimeInMillis();
+            }
+        },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    @OnClick(R.id.to)
+    void onToClick(View view){
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year,month,day);
+                to=calendar.getTimeInMillis();
+            }
+        },calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    @OnClick(R.id.save)
+    void onSaveClick(View view){
+        SharedPreferences preferences = getSharedPreferences("common",MODE_PRIVATE);
+        preferences.edit().putLong("from",from).apply();
+        preferences.edit().putLong("to",to).apply();
+        finish();
+    }
+
 }
--- app/src/main/java/the_martian_chronicles/android/the_martian_chronicles/weatherModule/WeatherFragment.java
+++ app/src/main/java/the_martian_chronicles/android/the_martian_chronicles/weatherModule/WeatherFragment.java
@@ -3,6 +3,7 @@ package the_martian_chronicles.android.the_martian_chronicles.weatherModule;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -13,9 +14,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kaopiz.kprogresshud.KProgressHUD;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -26,6 +31,7 @@ import the_martian_chronicles.android.the_martian_chronicles.App;
 import the_martian_chronicles.android.the_martian_chronicles.DateActivity;
 import the_martian_chronicles.android.the_martian_chronicles.R;
 import the_martian_chronicles.android.the_martian_chronicles.models.OpenWeather;
+import the_martian_chronicles.android.the_martian_chronicles.models.WeatherInfo;
 import the_martian_chronicles.android.the_martian_chronicles.services.WeatherApi;
 
 /**
@@ -46,6 +52,8 @@ public class WeatherFragment extends Fragment implements WeatherContract.View{
     RecyclerView recyclerView;
     @BindView(R.id.fab)
     FloatingActionButton floatingActionButton;
+    @BindView(R.id.holder)
+    TextView holder;
 
     KProgressHUD dialog;
 
@@ -109,13 +117,26 @@ public class WeatherFragment extends Fragment implements WeatherContract.View{
     @Override
     public void showError() {
         if(getActivity()!=null){
-            Toast.makeText(getActivity(), "Network error.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), "Network error", Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     public void showWeather(OpenWeather openWeather) {
-        Log.d("log ", "showWeather: count "+openWeather.getList().size());
-        recyclerView.setAdapter(new WeatherAdapter(openWeather.getList()));
+        SharedPreferences preferences = getActivity().getSharedPreferences("common",Context.MODE_PRIVATE);
+        long from = preferences.getLong("from", System.currentTimeMillis());
+        long to = preferences.getLong("to", System.currentTimeMillis()*2)+60*60*24*1000;
+        List<WeatherInfo> list = openWeather.getList();
+        List<WeatherInfo> filteredList=new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).getDt()*1000L>=from && list.get(i).getDt()*1000L<=to){
+                filteredList.add(list.get(i));
+            }
+        }
+        if (filteredList.size()==0)
+            holder.setVisibility(TextView.VISIBLE);
+        else
+            holder.setVisibility(TextView.GONE);
+        recyclerView.setAdapter(new WeatherAdapter(filteredList));
     }
 }
