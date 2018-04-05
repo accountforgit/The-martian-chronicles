fa870f17d53bbe301aa27caa129070aa9421d115
nurzhigit.shalkarbai@ce.sdu.edu.kz <asd>
2018-04-05 15:20:19 +0600

    add dependencys

---
+++ app/src/main/java/the_martian_chronicles/android/the_martian_chronicles/StartActivity.java
@@ -0,0 +1,13 @@
+package the_martian_chronicles.android.the_martian_chronicles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
